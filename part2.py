9c6f83e46128070a2b5ab635d9c1048d379e6e32
Pascal Zingg <pazingg@gmail.com>
2017-05-20 08:04:11 -0700

    Fix in calculation

--- part2.py
+++ part2.py
@@ -19,21 +19,29 @@ rdd = rdd.filter(lambda x: not x.startswith("code,"))
 # As value we are using the timestamp
 def mapf(x):
     fields = x.split(",")
-    return (fields[1] + "_" + fields[2], int(fields[6]))
+    return (fields[1] + "_" + fields[2], (int(fields[6]), int(fields[6])))
 
 
 # Map the function above and reduce by combined key to get the diff
 # between two corresponding request and response entries
 rddReqResp = rdd.map(mapf)
-rddReqRespTimeDiff = rddReqResp.reduceByKey(lambda x, y: y - x)
+rddReqRespTimeDiff = rddReqResp.reduceByKey(lambda x, y: (x[0] / 1000 * 60, y[1] - x[1]))
 
-# To get the
-rddReqMinute = rddReqResp.reduceByKey(lambda x, y: x / 1000 * 60)
-rddReqMinuteSum = rddReqMinute.reduceByKey(operator.add)
+# Remove key from rdd and calculate the sum of the requests within one minute
+rddReqRespMapped = rddReqRespTimeDiff.map(lambda x: x[1])
+rddReducedByMinute = rddReqRespMapped.reduceByKey(operator.add)
+
+# Sum up all the values for each minute (not a necessary step, summation can
+# be done before)
+resultSum = rddReducedByMinute.map(lambda x: x[1]).reduce(operator.add) 
+
+# Calcuate the average response time for requests inside a minute
+#minutes = rddReducedByMinute.max()[0] - rddReducedByMinute.min()[0]
+print(resultSum / rddReducedByMinute.count())
 
 # Write the throughput for exsiting minutes to csv file. Note: the
 # minutes which does not contains any entries are not inlcuded
-with open("out.csv", "wb") as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    for line in rddReqMinute.collect():
-        writer.writerow(line)
+# with open("out.csv", "wb") as csv_file:
+#     writer = csv.writer(csv_file, delimiter=',')
+#     for line in rddReqMinute.collect():
+#         writer.writerow(line)
