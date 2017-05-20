b0b9250a23862e351791632ef9546a029343bcd4
Pascal Zingg <pazingg@gmail.com>
2017-05-20 10:02:25 +0200

    Formatting

--- part1.py
+++ part1.py
@@ -16,21 +16,22 @@ rdd = sc.textFile("/user/hduser/mw_trace50.csv")
 # Filter by res_snd
 rddSend = rdd.filter(lambda w: w.startswith("res_snd,"))
 
+
 # Get minutes from csv file, located at the fift column. To get
-# distinct values and calculate the occurence inside a minute, 
+# distinct values and calculate the occurence inside a minute,
 # we define a tumple with value 1 to sum up the values later
-minutef = lambda x: (str(int(x.split(",")[6]) / 1000 * 60), 1)
+def minutef(x):
+    return (str(int(x.split(",")[6]) / 1000 * 60), 1)
+
 
 # Execute the mapping function and reduce by key. The latter will
 # sum up all the values which are grouped by one key and put them
 # into a tuple -> (<min>, <occurence>)
 rddDistinctMin = rddSend.map(minutef).reduceByKey(operator.add)
 
-	
 # Now we have to sum up all the occurences inside one minute and
 # divide them through the minutes occurence
 result = rddDistinctMin.map(lambda x: int(x[1])).reduce(operator.add)
-
 
 # To get the throughput per minte, we have to calculate the difference of
 # the minutes range
@@ -40,10 +41,9 @@ maxTime = int(rddDistinctMin.max()[0])
 diff = maxTime - minTime
 print(result / diff)
 
-
 # Write the throughput for exsiting minutes to csv file. Note: the
 # minutes which does not contains any entries are not inlcuded
 with open("out.csv", "wb") as csv_file:
-	writer = csv.writer(csv_file, delimiter=',')
-	for line in rddDistinctMin.collect():
-		writer.writerow(line) 
+    writer = csv.writer(csv_file, delimiter=',')
+    for line in rddDistinctMin.collect():
+        writer.writerow(line)
--- part2.py
+++ part2.py
@@ -14,25 +14,26 @@ sc.setLogLevel("ERROR")
 rdd = sc.textFile("/user/hduser/client_trace50.csv")
 rdd = rdd.filter(lambda x: not x.startswith("code,"))
 
+
 # Defines mapping function for combined key (client_id and loc_ts).
 # As value we are using the timestamp
 def mapf(x):
     fields = x.split(",")
     return (fields[1] + "_" + fields[2], int(fields[6]))
 
+
 # Map the function above and reduce by combined key to get the diff
 # between two corresponding request and response entries
 rddReqResp = rdd.map(mapf)
-rddReqRespTimeDiff = rddReqResp.reduceByKey(lambda x,y: y - x) 
+rddReqRespTimeDiff = rddReqResp.reduceByKey(lambda x, y: y - x)
 
-# To get the 
-rddReqMinute = rddReqResp.reduceByKey(lambda x,y: x / 1000 * 60)
+# To get the
+rddReqMinute = rddReqResp.reduceByKey(lambda x, y: x / 1000 * 60)
 rddReqMinuteSum = rddReqMinute.reduceByKey(operator.add)
-
 
 # Write the throughput for exsiting minutes to csv file. Note: the
 # minutes which does not contains any entries are not inlcuded
 with open("out.csv", "wb") as csv_file:
-	writer = csv.writer(csv_file, delimiter=',')
-	for line in rddReqMinute.collect():
-		writer.writerow(line) 
+    writer = csv.writer(csv_file, delimiter=',')
+    for line in rddReqMinute.collect():
+        writer.writerow(line)
